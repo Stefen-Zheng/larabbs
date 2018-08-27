d4b1492172873fbb777849344620148ed69aa86e
Stefen-Zheng <stefen-zheng@foxmail.com>
2018-08-27 10:39:45 +0000

    3.1-20180827

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://weibo.com/u/6361126868" target="_blank">Stefen</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="stefen-zheng@foxmail.com">联系我们</a></p>
+    </div>
+</footer>
