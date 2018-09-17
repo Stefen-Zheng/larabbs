118ac21a36dfcb587e586163fc86d514551d4457
Stefen-Zheng <stefen-zheng@foxmail.com>
2018-09-17 06:36:38 +0000

    Basic user profile editing

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -32,4 +32,16 @@ class CreateUsersTable extends Migration
     {
         Schema::dropIfExists('users');
     }
+    
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
 }
